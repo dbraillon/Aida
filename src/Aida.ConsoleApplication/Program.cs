07dfb06b7f6657f59c965e854894c605df31edc8
Davy Braillon <davy.braillon@live.com>
2016-10-27 08:25:15 +0200

    First commit, creating VS solution and sample projects

---
+++ src/Aida.ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aida.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aida.ConsoleApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Voice voice = new Voice(CultureInfo.CurrentCulture);
+            voice.Say("Hello, I'm glad to see you back!");
+        }
+    }
+}
---
+++ src/Aida.Core/Voice.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Speech.Synthesis;
+
+namespace Aida.Core
+{
+    public class Voice
+    {
+        protected SpeechSynthesizer Synthesizer { get; set; }
+
+        public Voice(CultureInfo cultureInfo)
+        {
+            Synthesizer = new SpeechSynthesizer();
+            Synthesizer.SetOutputToDefaultAudioDevice();
+            Synthesizer.Rate = 1;
+            Synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 0, cultureInfo);
+        }
+
+        public void Say(string message)
+        {
+            Synthesizer.Speak(message);
+        }
+    }
+}
