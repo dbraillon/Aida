d9158c8b4d06dc37e489f50669c3c278b8b6f2a4
Davy Braillon <davy.braillon@live.com>
2016-10-31 22:31:55 +0100

    Now download every day an update of Aida, but not install it yet

--- src/Aida.WindowsServices/UpdateService.cs
+++ src/Aida.WindowsServices/UpdateService.cs
@@ -6,16 +6,23 @@ using System.Configuration.Install;
 using System.Data;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Aida.WindowsServices
 {
     public partial class UpdateService : ServiceBase
     {
+        protected DirectoryInfo ApplicationDirectory { get; set; }
+        protected Thread ServiceThread { get; set; }
+        protected volatile bool IsRunning;
+
         public UpdateService()
         {
             InitializeComponent();
@@ -23,12 +30,33 @@ namespace Aida.WindowsServices
 
         protected override void OnStart(string[] args)
         {
-            Voice v = new Voice(CultureInfo.CurrentCulture);
-            v.Say("Hello! I'm glad to see you again!");
+            ApplicationDirectory = Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Application"));
+            IsRunning = true;
+            ServiceThread = new Thread(Loop);
+            ServiceThread.Start();
         }
 
         protected override void OnStop()
         {
+            IsRunning = false;
+            ServiceThread.Join();
+        }
+
+        protected void Loop()
+        {
+            while (IsRunning)
+            {
+                var url = "https://github.com/dbraillon/Aida/releases/download/last-version/last.zip";
+                var zipFile = Path.Combine(ApplicationDirectory.FullName, "last.zip");
+
+                using (var webClient = new WebClient())
+                {
+                    webClient.DownloadFile(url, zipFile);
+                }
+
+                // Wait one day
+                Thread.Sleep(1000 * 60 * 60 * 24);
+            }
         }
     }
 }
